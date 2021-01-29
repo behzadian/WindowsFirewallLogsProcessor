38029c3598f44bc6a94791f9a34f943684f1a210
Homayoun <behzadian@gmail.com>
2021-01-29 20:28:17 +0330

    trying to add search

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Collections.Concurrent;
 
 namespace EventParser
 {
@@ -29,11 +30,13 @@ namespace EventParser
 			public bool LogBlockedApp { get; set; }
 			public int Max { get; set; }
 			public bool LiveLog { get; set; }
+			public string Query { get; internal set; }
 		}
 
 		BackgroundWorker TheBackgroundWorker;
 		private void StartButton_Click(object sender, RoutedEventArgs e) {
 			workerConfigs configs = new workerConfigs();
+			configs.Query = "firefox";
 			configs.LogPermittedApps = PermittedsCheckBox.IsChecked.GetValueOrDefault();
 			configs.LogBlockedApp = BlockedsCheckBox.IsChecked.GetValueOrDefault();
 			configs.LiveLog = LiveLogsCheckBox.IsChecked.GetValueOrDefault();
@@ -70,8 +73,8 @@ namespace EventParser
 			if (e.UserState is List<string>) {
 				OutputTextBlock.Text = (e.UserState as List<string>).Join("\r\n");
 			}
-			if (e.UserState is Dictionary<string, List<string>>) {
-				var taskApps = e.UserState as Dictionary<string, List<string>>;
+			if (e.UserState is ConcurrentDictionary<string, List<string>>) {
+				var taskApps = e.UserState as ConcurrentDictionary<string, List<string>>;
 				string result = "";
 				bool displayPermittedConnections = PermittedsCheckBox.IsChecked ?? false;
 				bool displayBlockedConnections = BlockedsCheckBox.IsChecked ?? false;
@@ -95,10 +98,15 @@ namespace EventParser
 			List<string> messages = new List<string>();
 			workerConfigs configs = (workerConfigs)e.Argument;
 			List<string> apps = new List<string>();
-			Dictionary<string, List<string>> taskApps = new Dictionary<string, List<string>>();
+			ConcurrentDictionary<string, List<string>> taskApps = new ConcurrentDictionary<string, List<string>>();
 			int parsed = 0;
 			if (!configs.LogBlockedApp && !configs.LogPermittedApps)
 				return;
+
+			bool query = configs.Query.IsUsable();
+			string q = configs.Query.ToLower();
+			var destinationAddressPattern = new Regex("Destination Address\\:	(?<ip>(\\d{1,3}\\.?){4})");
+
 			using (var reader = new EventLogReader(@"C:\WINDOWS\System32\winevt\Logs\Security.evtx", PathType.FilePath)) {
 				EventRecord record;
 				while ((record = reader.ReadEvent()) != null && parsed < configs.Max) {
@@ -115,55 +123,69 @@ namespace EventParser
 						if (configs.LiveLog)
 							TheBackgroundWorker.ReportProgress(parsed * 100 / configs.Max, description);
 						var firstLine = description.SplitToNonEmptyParts("\n\n").First();
-						bool firewallBlocked = description.StartsWith("The Windows Filtering Platform has blocked a connection.");
-						bool firewallPermitted = description.StartsWith("The Windows Filtering Platform has permitted a connection.");
 
-						//if (!configs.LogBlockedApp) {
-						//	if (firewallBlocked) {
-						//		Logger.Value.Debug($"Parsed: {parsed}. Skip because App bloked but config does not allow us to process blocked apps");
-						//		continue;
-						//	}
-						//}
+						if (query) {
+							if (description.ToLower().Contains(q)) {
+								if (!taskApps.ContainsKey("Query:"))
+									taskApps.AddOrUpdate("Query:", new List<string>(), (x, y) => y);
+								var match = destinationAddressPattern.Match(description);
+								if (match.Success) {
+									var ip = match.Groups["ip"].Value;
+									if (!taskApps["Query:"].Contains(ip))
+										taskApps["Query:"].Add(ip);
+								}
+								TheBackgroundWorker.ReportProgress(parsed * 100 / configs.Max, taskApps);
+							}
+						} else {
+							bool firewallBlocked = description.StartsWith("The Windows Filtering Platform has blocked a connection.");
+							bool firewallPermitted = description.StartsWith("The Windows Filtering Platform has permitted a connection.");
+							//if (!configs.LogBlockedApp) {
+							//	if (firewallBlocked) {
+							//		Logger.Value.Debug($"Parsed: {parsed}. Skip because App bloked but config does not allow us to process blocked apps");
+							//		continue;
+							//	}
+							//}
 
-						//if (!configs.LogPermittedApps) {
-						//	if (firewallPermitted) {
-						//		Logger.Value.Debug($"Parsed: {parsed}. Skip because App permitted but config does not allow us to process permitted apps");
-						//		continue;
-						//	}
-						//}
+							//if (!configs.LogPermittedApps) {
+							//	if (firewallPermitted) {
+							//		Logger.Value.Debug($"Parsed: {parsed}. Skip because App permitted but config does not allow us to process permitted apps");
+							//		continue;
+							//	}
+							//}
 
-						var app = Regex.Match(description, "Application Name\\:	(?<app>[^\n\r]*)").Groups["app"].Value;
-						if (app.IsUsable()) {
-							for (int counter = 0; counter < 10; counter++) {
-								if (app.StartsWith($@"\device\harddiskvolume{counter}\")) {
-									app = app.Substring($@"\device\harddiskvolume{counter}\".Length);
-									char[] drives = "abcdefghijklmnopqrstuvxyz".ToArray();
-									foreach (var drive in drives) {
-										if (File.Exists(drive + ":\\" + app)) {
-											app = drive + ":\\" + app;
-											app = new FileInfo(app).FullName;
-											break;
+							var app = Regex.Match(description, "Application Name\\:	(?<app>[^\n\r]*)").Groups["app"].Value;
+							if (app.IsUsable()) {
+								for (int counter = 0; counter < 10; counter++) {
+									if (app.StartsWith($@"\device\harddiskvolume{counter}\")) {
+										app = app.Substring($@"\device\harddiskvolume{counter}\".Length);
+										char[] drives = "abcdefghijklmnopqrstuvxyz".ToArray();
+										foreach (var drive in drives) {
+											if (File.Exists(drive + ":\\" + app)) {
+												app = drive + ":\\" + app;
+												app = new FileInfo(app).FullName;
+												break;
+											}
 										}
 									}
 								}
+
+								//if (firewallPermitted)
+								//	app = "(Permitted)\t" + app;
+								//if (firewallBlocked)
+								//	app = "(Blocked)\t" + app;
+								//if (!apps.Contains(app))
+								//	apps.Add(app);
+								if (!taskApps.ContainsKey(firstLine))
+									taskApps.AddOrUpdate(firstLine, new List<string>(), (x, y) => y);
+								if (!taskApps[firstLine].Contains(app))
+									taskApps[firstLine].Add(app);
+
+								Logger.Value.Info($"Parsed: {parsed}. Adding {app}");
+							} else {
+								Logger.Value.Debug($"Parsed: {parsed}. App is null");
 							}
-
-							//if (firewallPermitted)
-							//	app = "(Permitted)\t" + app;
-							//if (firewallBlocked)
-							//	app = "(Blocked)\t" + app;
-							//if (!apps.Contains(app))
-							//	apps.Add(app);
-							if (!taskApps.ContainsKey(firstLine))
-								taskApps.Add(firstLine, new List<string>());
-							if (!taskApps[firstLine].Contains(app))
-								taskApps[firstLine].Add(app);
-
-							Logger.Value.Info($"Parsed: {parsed}. Adding {app}");
-						} else {
-							Logger.Value.Debug($"Parsed: {parsed}. App is null");
+							TheBackgroundWorker.ReportProgress(parsed * 100 / configs.Max, taskApps);
 						}
-						TheBackgroundWorker.ReportProgress(parsed * 100 / configs.Max, taskApps);
 					}
 				}
 			}
